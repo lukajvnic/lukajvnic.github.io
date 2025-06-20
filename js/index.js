00c69ed1510c39f4e6414f4533a7fe90c77c22e0
nelimalu <lukaj2501@gmail.com>
2025-06-20 12:57:58 -0400

    created canvas

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-const TIME_URL = "http://worldtimeapi.org/api/timezone/America/Toronto"
+const TIME_URL = "http://worldtimeapi.org/api/timezone/America/Toronto";
+const mouse = {
+  x: 0,
+  y: 0
+}
+const BRIGHTNESS = " .:-=+*#%@";
 
 
 
@@ -8,15 +13,51 @@ window.addEventListener("load", (event) => {
   fetch('http://worldtimeapi.org/api/timezone/America/Toronto')
   .then(res => res.json())
   .then(post => {
-    console.log(post);  // logs the title of post #1
+    // console.log(post);  // logs the title of post #1
 
     let time_abbrev = document.getElementById("time-abbreviation");
     time_abbrev.innerHTML = post.abbreviation;
   })
   .catch(err => console.error('Error fetching post:', err));
+  
+  fillArt(); 
 
   setInterval(loop, 100);
 });
+
+
+window.addEventListener("mousemove", (event) => {
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
+});
+
+function setPixel(x, y, value) {
+  const element = document.getElementsByClassName(`r${y} c${x}`)[0];
+  element.innerHTML = value;
+}
+
+function map(value, in_min, in_max, out_min, out_max) {
+  let percentage = (value - in_min) / (in_max - in_min);
+  return (out_max - out_min) * percentage + out_min;
+}
+
+function fillArt() {
+  let art_container = document.getElementById("art");
+  const num_rows = getComputedStyle(art_container)
+                   .getPropertyValue('grid-template-rows')
+                   .split(' ').length;
+
+  for (let i = 0; i < num_rows; i++) {
+    for (let j = 0; j < num_rows; j++) {
+      if (Math.sqrt(i ** 2 + j ** 2) < 15)
+        art_container.innerHTML += `<div class="r${i} c${j} pixel">o</div>`;
+      else
+        art_container.innerHTML += `<div class="r${i} c${j} pixel">x</div>`;
+
+    }
+  }
+
+}
 
 
 function loop() {
@@ -24,6 +65,16 @@ function loop() {
 
   let time = document.getElementById("time");
   let ampm = document.getElementById("ampm");
+  let artrect = document.getElementById("art").getBoundingClientRect();
+  let art_pos = {
+    x: (artrect.right + artrect.left) / 2,
+    y: (artrect.top + artrect.bottom) / 2
+  };
+
+  let angle = getAngle(mouse, art_pos);
+  // console.log(angle * 180 / Math.PI);
+
+  setPixel(3, 3, 'x');
 
   const torontoTime = now.toLocaleString('en-CA', {
     timeZone: 'America/Toronto',
@@ -32,4 +83,9 @@ function loop() {
 
   time.innerHTML = torontoTime[1];
   ampm.innerHTML = torontoTime[2][0].toUpperCase() + torontoTime[2][2].toUpperCase();
+}
+
+
+function getAngle(o1, o2) {
+  return Math.PI - Math.atan2(o2.y - o1.y, o2.x - o1.x);
 }
