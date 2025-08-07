a49a28f952c5e7f44448e7e576e84c95c0c39115
nelimalu <lukaj2501@gmail.com>
2025-08-06 21:03:22 -0400

    increased shark resolution, added colours, changed x function

--- js/index.js
+++ js/index.js
@@ -3,16 +3,28 @@ const mouse = {
   x: 0,
   y: 0
 }
-//                  .:-=+#%@
+
 const ASCII_MAP = " ·-:;coxa";
-var prev_location = "340;-25";
+var prev_location = "319;20";
 var num_rows = 36;
 
+// blue colours for shark
+const COLOURS = [
+  "#B8DDEB",
+  "#A3D2E7",
+  "#8DC6E0",
+  "#78BAD9",
+  "#64ADD1",
+  "#509FC9",
+  "#3C91BF",
+  "#2F83B2",
+  "#2275A4"
+];
 
 window.addEventListener("load", (event) => {
 
   // get timezone in toronto
-  fetch('http://worldtimeapi.org/api/timezone/America/Toronto')
+  fetch('https://worldtimeapi.org/api/timezone/America/Toronto')
   .then(res => res.json())
   .then(post => {
     // console.log(post);  // logs the title of post #1
@@ -22,9 +34,8 @@ window.addEventListener("load", (event) => {
   })
   .catch(err => console.error('Error fetching post:', err));
   
+  // load art display
   fillArt(); 
-
-  setInterval(loop, 100);
 });
 
 
@@ -36,6 +47,7 @@ window.addEventListener("mousemove", (event) => {
 function setPixel(x, y, value) {
   const element = document.getElementsByClassName(`r${y} c${x}`)[0];
   element.innerHTML = value;
+  element.style.color = COLOURS[ASCII_MAP.indexOf(value)];
 }
 
 function map(value, in_min, in_max, out_min, out_max) {
@@ -49,34 +61,23 @@ function fillArt() {
                    .getPropertyValue('grid-template-rows')
                    .split(' ').length;
 
+  // create grid for drawing
   for (let i = 0; i < num_rows; i++) {
     for (let j = 0; j < num_rows; j++) {
-      art_container.innerHTML += `<div class="r${i} c${j} pixel"> </div>`;
+      if (i == parseInt(num_rows / 2) && i == j)
+        art_container.innerHTML += `<div class="r${i} c${j} pixel">?</div>`;
+      else
+        art_container.innerHTML += `<div class="r${i} c${j} pixel"> </div>`;
     }
   }
 
-  // x o a . : ; - 
-  // · - : ; c o a x
-  // let chars = "a c e n o u v x z . : ; - · °".split(' ')
-  // count = 0;
-  // for (let char of chars) {
-  //   setPixel(count, 0, char);
-  //   count++;
-  // }
-
-  // for (const key in shark['340;-25'].data) {
-  //   for (let position of shark['340;-25'].data[key]) {
-  //     setPixel(position[0], position[1], ASCII_MAP[parseInt(key)])
-  //   }
-  // }
-
+  // start main loop
+  setInterval(loop, 100);
 }
 
-
+// cosine func for manipulating x position of mouse
 function compress(x) {
-  let a = Math.pow(window.innerWidth / 2, 2/3);
-  let out = a * Math.cbrt(x - (window.innerWidth / 2)) + (window.innerWidth / 2);
-  return out;
+  return -(window.innerWidth / 2) * Math.cos((Math.PI * x) / window.innerWidth) + (window.innerWidth / 2);
 }
 
 
@@ -85,18 +86,18 @@ function loop() {
 
   let time = document.getElementById("time");
   let ampm = document.getElementById("ampm");
-  let artrect = document.getElementById("art").getBoundingClientRect();
-  let art_pos = {
-    x: (artrect.right + artrect.left) / 2,
-    y: (artrect.top + artrect.bottom) / 2
-  };
-
+  // let artrect = document.getElementById("art").getBoundingClientRect();
+  // let art_pos = {
+  //   x: (artrect.right + artrect.left) / 2,
+  //   y: (artrect.top + artrect.bottom) / 2
+  // };
 
   const torontoTime = now.toLocaleString('en-CA', {
     timeZone: 'America/Toronto',
-    hour12: true, // 24-hour format; set to true if you want AM/PM
+    hour12: true,
   }).split(' ');
 
+  // update live time
   time.innerHTML = torontoTime[1];
   ampm.innerHTML = torontoTime[2][0].toUpperCase() + torontoTime[2][2].toUpperCase();
 
@@ -104,27 +105,28 @@ function loop() {
   const width = window.innerWidth;
   const height = window.innerHeight;
 
-  const alpha = parseInt((parseInt(map(compress(mouse.x), 0, width, 340, 200)) - 1) / 3) * 3 + 1;
+  // get alpha and beta angles of shart based on mouse position
+  const alpha = parseInt((parseInt(map(compress(mouse.x), 0, width, 340, 218)) - 1) / 3) * 3 + 1;
   const beta = parseInt(parseInt(map(mouse.y, 0, height, 0, 50)) / 3) * 3 - 25;
-
-
 
   const main_key = `${alpha};${beta}`
 
-    for (const key in shark[prev_location].data) {
-      for (let position of shark[prev_location].data[key]) {
-        let x = parseInt(map(position[0], 0, 24, 0, num_rows - 1));
-        let y = parseInt(map(position[1], 0, 24, 0, num_rows - 1));
+  // erase previous shark
+  for (const key in shark[prev_location].data) {
+    for (let position of shark[prev_location].data[key]) {
+      let x = parseInt(map(position[0], 0, 50, 0, num_rows - 1));
+      let y = parseInt(map(position[1], 0, 50, 0, num_rows - 1));
 
-        setPixel(x, y, ' ')
-      }
+      setPixel(x, y, ' ')
     }
+  }
 
+  // draw new shark
   try {
     for (const key in shark[main_key].data) {
       for (let position of shark[main_key].data[key]) {
-        let x = parseInt(map(position[0], 0, 24, 0, num_rows - 1));
-        let y = parseInt(map(position[1], 0, 24, 0, num_rows - 1));
+        let x = parseInt(map(position[0], 0, 50, 0, num_rows - 1));
+        let y = parseInt(map(position[1], 0, 50, 0, num_rows - 1));
         // figure out how to check if pixel is next to a dead pixel and draw it too
 
         setPixel(x, y, ASCII_MAP[parseInt(key)])
@@ -133,12 +135,13 @@ function loop() {
 
     prev_location = main_key;
   } catch (e) {
-
+    // if drawing fails, draw the previous shark
+    for (const key in shark[prev_location].data) {
+      for (let position of shark[prev_location].data[key]) {
+        let x = parseInt(map(position[0], 0, 50, 0, num_rows - 1));
+        let y = parseInt(map(position[1], 0, 50, 0, num_rows - 1));
+        setPixel(x, y, ASCII_MAP[parseInt(key)])
+      }
+    }
   }
-
-}
-
-
-function getAngle(o1, o2) {
-  return Math.PI - Math.atan2(o2.y - o1.y, o2.x - o1.x);
 }
